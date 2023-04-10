45a95a6753825ef61e38c194ccc26db4fe53abcf
akezeth <116703083+akezeth@users.noreply.github.com>
2023-04-10 17:13:30 +0000

    Update Linter Configuration

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-/*============================= toggle hamburger icon ===========================*/
-let menuIcon = document.querySelector('#menuIcon');
-let navbar = document.querySelector('.navbar');
+// ============================= toggle hamburger icon ===========================
+const menuIcon = document.querySelector('#menuIcon');
+const navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
   menuIcon.classList.toggle('uil-multiply');
   navbar.classList.toggle('active');
 };
 
-/*========= remove toggle icon and navbar when user click navbar link (scroll) =========*/
+// ========= remove toggle icon and navbar when user click navbar link (scroll) =========
 window.onscroll = () => {
   menuIcon.classList.remove('uil-multiply');
   navbar.classList.remove('active');
