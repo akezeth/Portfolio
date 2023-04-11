c766425c8a11fb1f43c12395166d42a90f181724
akezeth <116703083+akezeth@users.noreply.github.com>
2023-04-11 08:41:15 +0000

    Resolve linter errors

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const menuIcon = document.querySelector('#menuIcon');
 const navbar = document.querySelector('.navbar');
 const logo = document.querySelector('.logo');
 
-
 menuIcon.onclick = () => {
   menuIcon.classList.toggle('uil-multiply');
   navbar.classList.toggle('active');
