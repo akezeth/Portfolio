02f036b6f4c55c0a320f93dd0408e926bda370be
akezeth <116703083+akezeth@users.noreply.github.com>
2023-04-19 16:33:20 +0000

    Adjust linter errors

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,17 +1,12 @@
 const form = document.getElementById('form');
 const email = document.getElementById('email');
-const submit = document.getElementById('submit');
 const errorMessage = document.querySelector('.errorMessage');
-
-
 
 form.addEventListener('submit', (e) => {
   if (email.value !== email.value.toLowerCase()) {
     e.preventDefault();
     errorMessage.textContent = 'Your email input must be in lowercases!';
-    console.log("Aklilu Mekonnen!");
-  }
-  else {
+  } else {
     errorMessage.textContent = '';
   }
 });
