9384bd59db6b771c6f05a2f4ae00e9425e09b845
coker2019 <olabisi2k11@gmail.com>
2023-04-20 11:37:16 +0100

    Finalized Local storage.

--- js/localStorage.js
+++ js/localStorage.js
@@ -18,3 +18,13 @@ function loadLocalStorage() {
     });
   }
 }
+formInputsArr.forEach((formInput) => {
+  formInput.addEventListener('keyup', (e) => {
+    const input = e.target.attributes.name.value;
+    formStorage[input] = e.target.value;
+    const formStorageStr = JSON.stringify(formStorage);
+    localStorage.setItem('formData', formStorageStr);
+  });
+});
+
+window.addEventListener('load', loadLocalStorage);
